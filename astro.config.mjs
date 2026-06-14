f742e77e0e74dbd140f070e6afe8d762c290a788
kbsoo <kbsoo0620@gmail.com>
2026-06-14 19:58:27 +0900

    feat(blog): add math rendering and declaration style

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,17 @@
 import { defineConfig } from "astro/config";
+import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
 
 export default defineConfig({
   site: "https://kbsooo.github.io",
   output: "static",
   publicDir: "./static",
   markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [[rehypeKatex, { strict: false }]],
     shikiConfig: {
       theme: "github-dark",
       wrap: true,
     },
   },
 });
-
